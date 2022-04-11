9cc0cbcdd0300b681e3b0eb3ec719ff9a516e569
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-04-12 08:25:49 +0900

    implements univese and cell

--- src/lib.rs
+++ src/lib.rs
@@ -13,9 +13,29 @@ extern "C" {
     fn alert(s: &str);
 }
 
+// #[wasm_bindgen]
+// pub fn greet(hoge: &str) {
+//     alert("aaa");
+//     alert(&format!("Hello, {}", hoge));
+// }
+
 #[wasm_bindgen]
-// pub fn greet(_hoge: &str) {
-pub fn greet() {
-    alert("aaa");
-    // alert(&format!("Hello, {}", hoge));
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+impl Universe {
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
 }
